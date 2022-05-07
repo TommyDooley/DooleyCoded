752dde1f95844eb942833d9f8af02cf49506a31f
TommyDooley <105026585+TommyDooley@users.noreply.github.com>
2022-05-07 16:41:27 -0400

    Add files via upload

---
+++ ASCII_art.py
@@ -0,0 +1,24 @@
+#Name = Tommy Dooley
+#I enjoy learning how to code!
+
+T = """
+TTTTT
+  T
+  T
+  T
+  T
+  T
+  T
+"""
+print(T)
+
+D = """
+DDDD
+D   D
+D   D
+D   D
+D   D
+D   D
+DDDD
+"""
+print(D)
