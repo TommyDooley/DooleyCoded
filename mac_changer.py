5118e369124e0fd3db3259b6bef0c1c79d82fcd4
Tommy Dooley II <105026585+TommyDooley@users.noreply.github.com>
2022-05-31 20:46:09 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python3
-
+# #!/usr/bin/env python3
+# 
 # import subprocess
 # import argparse
-
+# 
+# parser = argparse.ArgumentParser()
+# 
+# parser.add_argument("-n", "--network", dest="network", metavar='', help="Select network device.")
+# parser.add_argument("-m", "--mac", dest="new_mac", metavar='', help="Select new MAC address.")
+# 
+# args = parser.parse_args()
+# 
 # def change_mac(network, new_mac):
 #     print(f"[+] Changing MAC address for {network} to {new_mac}")
-
+# 
 #     subprocess.call(["ifconfig", network, "down"])
 #     subprocess.call(["ifconfig", network, "hw", "ether", new_mac])
 #     subprocess.call(["ifconfig", network, "up"])
-
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument("-n", "--network", dest="network", help="Interface to change MAC address.")
-# parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address.")
-
-# args = parser.parse_args()
-
+# 
+# 
 # change_mac(args.network, args.new_mac)
